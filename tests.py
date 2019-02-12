2f626e175d8be1b944895d6bde38e5e3c1b03b21
Evan Sonderegger <esonderegger@nationaljournal.com>
2019-02-12 14:43:17 -0500

    fix regex for F1M

--- tests.py
+++ tests.py
@@ -80,6 +80,14 @@ class HandleF1FromWebForms(unittest.TestCase):
         self.assertIsInstance(parsed['filing']['effective_date'], datetime)
 
 
+class HandlePaperF1M(unittest.TestCase):
+    def test_request(self):
+        parsed = fecfile.from_http(1101469)
+        self.assertEqual(parsed['header']['fec_version'], 'P3.2')
+        self.assertEqual(parsed['filing']['city'], 'LUBBOCK')
+        self.assertIsInstance(parsed['filing']['date_signed'], datetime)
+
+
 class HandlePercentInNumber(unittest.TestCase):
     def test_request(self):
         parsed = fecfile.from_http(1235309)
@@ -304,6 +312,7 @@ if __name__ == '__main__':
         HasScheduleD('test_request'),
         HasScheduleI('test_request'),
         HandleF1FromWebForms('test_request'),
+        HandlePaperF1M('test_request'),
         HandlePercentInNumber('test_request'),
         HandleNANumber('test_request'),
         ConvertZipFileToJSON('test_convert'),
