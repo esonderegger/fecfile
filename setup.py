04b3cffa2fd118304352c3390bb691a84880627f
st421 <susan.tyler@washpost.com>
2021-09-02 11:03:58 -0400

    Misc. tiny changes for publishing a wapo-fecfile pypi package

--- setup.py
+++ setup.py
@@ -1,28 +1,35 @@
 from setuptools import setup, find_packages
 
-
+LONG_DESCRIPTION = ''
 with open('README.md', 'r') as file:
-    long_description = '\n'.join(file.readlines()[3:])
+    LONG_DESCRIPTION = '\n'.join(file.readlines()[3:])
 
-
-requirements = [
+INSTALL_REQUIRES = (
     'pytz>=2018.4',
-    'requests>=2.19.1',
-    ]
+    'requests<3'
+)
+
+PROJECT = 'wapo-fec'
+AUTHOR = 'The Washington Post Newsroom Engineering Team'
+COPYRIGHT = '2021, {}'.format(AUTHOR)
+
+# The full version, including alpha/beta/rc tags
+RELEASE = '0.0.2'
+# The short X.Y version
+VERSION = '.'.join(RELEASE.split('.')[:2])
 
 
 setup(
-    name='fecfile',
-    version='0.6.4',
+    name=PROJECT,
+    version=RELEASE,
     description='a python parser for the .fec file format',
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url='https://esonderegger.github.io/fecfile/',
     project_urls={
-        'Bug Tracker': 'https://github.com/esonderegger/fecfile/issues',
-        'Source Code': 'https://github.com/esonderegger/fecfile/',
+        'Bug Tracker': 'https://github.com/washingtonpost/fecfile/issues',
+        'Source Code': 'https://github.com/washingtonpost/fecfile/',
     },
-    author='Evan Sonderegger',
+    author=AUTHOR,
     author_email='evan@rpy.xyz',
     license='Apache License 2.0',
     keywords='fec campaign finance politics',
@@ -36,6 +43,6 @@ setup(
     ],
     packages=find_packages(exclude=['docs', 'test-data']),
     package_data={'fecfile': ['mappings.json', 'types.json']},
-    install_requires=requirements,
+    install_requires=INSTALL_REQUIRES,
     zip_safe=False,
-    )
+)
